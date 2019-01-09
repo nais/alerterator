2f11e38b09d81612d50626b5976a91ea48c5c26d
Kim Tore Jensen <kimtjen@gmail.com>
2019-01-09 11:17:49 +0100

    Add code generation tools

---
+++ hack/update-codegen.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+TEMP_DIR=`mktemp -d`
+SCRIPT_ROOT=$(dirname ${BASH_SOURCE})/..
+CODEGEN_PKG=$TEMP_DIR/cgen
+export GOPATH=~/go
+export GO111MODULE=on
+
+git clone -b release-1.11 https://github.com/kubernetes/code-generator $CODEGEN_PKG
+
+# generate the code with:
+# --output-base    because this script should also be able to run inside the vendor dir of
+#                  k8s.io/kubernetes. The output-base is needed for the generators to output into the vendor dir
+#                  instead of the $GOPATH directly. For normal projects this can be dropped.
+${CODEGEN_PKG}/generate-groups.sh "deepcopy,client,informer,lister" \
+  github.com/nais/alerterator/pkg/client github.com/nais/alerterator/pkg/apis \
+  alerterator:v1alpha1 \
+  --output-base ${TEMP_DIR} \
+  --go-header-file ${SCRIPT_ROOT}/hack/boilerplate.go.txt
+
+rsync -av ${TEMP_DIR}/github.com/nais/alerterator/ $SCRIPT_ROOT/
+
+rm -rf $TEMP_DIR
+
+# To use your own boilerplate text use:
+#   --go-header-file ${SCRIPT_ROOT}/hack/custom-boilerplate.go.txt
