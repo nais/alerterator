76a477ed0524f01f7a7f264adc8313f6fe9334b8
Kyrre Havik Eriksen <kyrremann@gmail.com>
2019-02-12 11:58:44 +0100

    Kjørt shellcheck, og oppdatert katalog-referanser

--- codegen/update-codegen.sh
+++ codegen/update-codegen.sh
@@ -4,8 +4,8 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-TEMP_DIR=`mktemp -d`
-SCRIPT_ROOT=$(dirname ${BASH_SOURCE})/..
+TEMP_DIR=$(mktemp -d)
+SCRIPT_ROOT=$(dirname "${BASH_SOURCE[0]}")/..
 CODEGEN_PKG=$TEMP_DIR/cgen
 
 export GO111MODULE=on
@@ -13,21 +13,21 @@ if [[ -z "${GOPATH}" ]]; then
     export GOPATH=~/go
 fi
 
-git clone -b release-1.11 https://github.com/kubernetes/code-generator $CODEGEN_PKG
+git clone -b release-1.11 "https://github.com/kubernetes/code-generator" "$CODEGEN_PKG"
 
 # generate the code with:
 # --output-base    because this script should also be able to run inside the vendor dir of
 #                  k8s.io/kubernetes. The output-base is needed for the generators to output into the vendor dir
 #                  instead of the $GOPATH directly. For normal projects this can be dropped.
-${CODEGEN_PKG}/generate-groups.sh "deepcopy,client,informer,lister" \
+"${CODEGEN_PKG}"/generate-groups.sh "deepcopy,client,informer,lister" \
   github.com/nais/alerterator/pkg/client github.com/nais/alerterator/pkg/apis \
   alerterator:v1alpha1 \
-  --output-base ${TEMP_DIR} \
-  --go-header-file ${SCRIPT_ROOT}/hack/boilerplate.go.txt
+  --output-base "${TEMP_DIR}" \
+  --go-header-file "${SCRIPT_ROOT}/codegen/boilerplate.go.txt"
 
-rsync -av ${TEMP_DIR}/github.com/nais/alerterator/ $SCRIPT_ROOT/
+rsync -av "${TEMP_DIR}/github.com/nais/alerterator/" "$SCRIPT_ROOT/"
 
-rm -rf $TEMP_DIR
+rm -rf "$TEMP_DIR"
 
 # To use your own boilerplate text use:
 #   --go-header-file ${SCRIPT_ROOT}/hack/custom-boilerplate.go.txt
