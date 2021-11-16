cfa814979174524b345293596cb24aaaf3958522
Kyrre Havik <kyrremann@gmail.com>
2021-11-16 13:23:42 +0100

    init: en start på hvordan vi jobber med configmap

--- controllers/alertmanager_configmap.go
+++ controllers/alertmanager_configmap.go
@@ -3,6 +3,7 @@ package controllers
 import (
 	"context"
 	"fmt"
+
 	naisiov1 "github.com/nais/liberator/pkg/apis/nais.io/v1"
 	v1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/types"
@@ -10,6 +11,7 @@ import (
 	"github.com/nais/alerterator/controllers/inhibitions"
 	"github.com/nais/alerterator/controllers/receivers"
 	"github.com/nais/alerterator/controllers/routes"
+	alertmanager "github.com/prometheus/alertmanager/config"
 	"gopkg.in/yaml.v2"
 )
 
@@ -24,7 +26,7 @@ var alertmanagerTemplateConfigMapName = types.NamespacedName{
 	Name:      "alertmanager-template-config",
 }
 
-func getConfig(ctx context.Context, namespacedName types.NamespacedName, alertReconciler *AlertReconciler) (map[interface{}]interface{}, error) {
+func getConfig(ctx context.Context, namespacedName types.NamespacedName, alertReconciler *AlertReconciler) (*alertmanager.Config, error) {
 	var configMap v1.ConfigMap
 	err := alertReconciler.Get(ctx, namespacedName, &configMap)
 	if err != nil {
@@ -35,13 +37,13 @@ func getConfig(ctx context.Context, namespacedName types.NamespacedName, alertRe
 		return nil, fmt.Errorf("alertmanager is not properly set up, data is empty")
 	}
 
-	config := make(map[interface{}]interface{})
+	config := alertmanager.Config{}
 	err = yaml.Unmarshal([]byte(configMap.Data[alertmanagerConfigName]), config)
 	if err != nil {
 		return nil, fmt.Errorf("failed while unmarshling %s: %s", alertmanagerConfigName, err)
 	}
 
-	return config, nil
+	return &config, nil
 }
 
 func updateConfigMap(ctx context.Context, namespacedName types.NamespacedName, config map[interface{}]interface{}, alertReconciler *AlertReconciler) error {
@@ -66,34 +68,34 @@ func updateConfigMap(ctx context.Context, namespacedName types.NamespacedName, c
 }
 
 func AddOrUpdateAlertmanagerConfigMap(ctx context.Context, alertReconciler *AlertReconciler, alert *naisiov1.Alert) error {
-	alertmanagerConfig, err := getConfig(ctx, alertmanagerConfigMapName, alertReconciler)
+	oldConfig, err := getConfig(ctx, alertmanagerConfigMapName, alertReconciler)
 	if err != nil {
 		return err
 	}
-	templateConfig, err := getConfig(ctx, alertmanagerTemplateConfigMapName, alertReconciler)
+	newConfig, err := getConfig(ctx, alertmanagerTemplateConfigMapName, alertReconciler)
 	if err != nil {
 		return err
 	}
 
-	updatedRoutes, err := routes.AddOrUpdateRoute(alert, alertmanagerConfig, templateConfig)
+	routes, err := routes.AddOrUpdateRoute(alert, oldConfig.Route.Routes)
 	if err != nil {
 		return fmt.Errorf("failed while adding/updating routes: %s", err)
 	}
-	templateConfig["route"] = updatedRoutes
+	newConfig.Route.Routes = routes
 
-	updatedReceivers, err := receivers.AddOrUpdateReceiver(alert, alertmanagerConfig)
+	updatedReceivers, err := receivers.AddOrUpdateReceiver(alert, oldConfig)
 	if err != nil {
 		return fmt.Errorf("failed while adding/updating receivers: %s", err)
 	}
-	templateConfig["receivers"] = updatedReceivers
+	newConfig["receivers"] = updatedReceivers
 
-	updatedInhibitRules, err := inhibitions.AddOrUpdateInhibition(alert, alertmanagerConfig)
+	updatedInhibitRules, err := inhibitions.AddOrUpdateInhibition(alert, oldConfig)
 	if err != nil {
 		return fmt.Errorf("failed while adding/updating inhibitions: %s", err)
 	}
-	templateConfig["inhibit_rules"] = updatedInhibitRules
+	newConfig["inhibit_rules"] = updatedInhibitRules
 
-	return updateConfigMap(ctx, alertmanagerConfigMapName, templateConfig, alertReconciler)
+	return updateConfigMap(ctx, alertmanagerConfigMapName, newConfig, alertReconciler)
 }
 
 func DeleteRouteAndReceiverFromAlertManagerConfigMap(ctx context.Context, alertReconciler *AlertReconciler, alert *naisiov1.Alert) error {
