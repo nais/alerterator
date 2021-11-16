b2b27d51a8a9c7ed9ac14e511898cba4e09caa91
Kyrre Havik <kyrremann@gmail.com>
2021-11-16 14:03:57 +0100

    feat: receivers skal fungere

--- controllers/receivers/receivers_test.go
+++ controllers/receivers/receivers_test.go
@@ -12,27 +12,29 @@ import (
 func TestReceivers(t *testing.T) {
 	t.Run("Validating that receivers are created correctly", func(t *testing.T) {
 		alert := fixtures.AlertResource()
-		receiver := createReceiver(alert)
-		assert.Equal(t, utils.GetCombinedName(alert), receiver.Name)
+		name := utils.GetCombinedName(alert)
+		receiver := createReceiver(name, alert)
+		assert.Equal(t, name, receiver.Name)
 		assert.Len(t, receiver.EmailConfigs, 1)
 		assert.Len(t, receiver.SlackConfigs, 1)
 
-		receivers := alert.Spec.Receivers
-		assert.Equal(t, receivers.Email.To, receiver.EmailConfigs[0].To)
-		assert.Equal(t, receivers.Email.SendResolved, receiver.EmailConfigs[0].SendResolved)
+		alertReceivers := alert.Spec.Receivers
+		assert.Equal(t, alertReceivers.Email.To, receiver.EmailConfigs[0].To)
+		assert.Equal(t, alertReceivers.Email.SendResolved, receiver.EmailConfigs[0].SendResolved())
 
-		assert.Equal(t, receivers.Slack.Channel, receiver.SlackConfigs[0].Channel)
-		assert.Equal(t, receivers.Slack.PrependText, alert.Spec.Receivers.Slack.PrependText)
-		assert.True(t, receiver.SlackConfigs[0].SendResolved)
+		assert.Equal(t, alertReceivers.Slack.Channel, receiver.SlackConfigs[0].Channel)
+		assert.Equal(t, alertReceivers.Slack.PrependText, alert.Spec.Receivers.Slack.PrependText)
+		assert.True(t, receiver.SlackConfigs[0].SendResolved())
 
-		assert.True(t, receiver.WebhookConfigs[0].SendResolved)
+		assert.True(t, receiver.WebhookConfigs[0].SendResolved())
 	})
 
 	t.Run("Valider at send_resolved for e-post blir beholdt", func(t *testing.T) {
 		alert := fixtures.AlertResource()
+		name := utils.GetCombinedName(alert)
 		alert.Spec.Receivers.Email.SendResolved = true
-		receiver := createReceiver(alert)
-		assert.True(t, receiver.EmailConfigs[0].SendResolved)
+		receiver := createReceiver(name, alert)
+		assert.True(t, receiver.EmailConfigs[0].SendResolved())
 	})
 
 	t.Run("Valider at send_resolved for sms blir beholdt", func(t *testing.T) {
@@ -40,9 +42,10 @@ func TestReceivers(t *testing.T) {
 			return &i
 		}
 		alert := fixtures.AlertResource()
+		name := utils.GetCombinedName(alert)
 		alert.Spec.Receivers.SMS.SendResolved = boolp(false)
-		receiver := createReceiver(alert)
-		assert.False(t, receiver.WebhookConfigs[0].SendResolved)
+		receiver := createReceiver(name, alert)
+		assert.False(t, receiver.WebhookConfigs[0].SendResolved())
 	})
 
 	t.Run("Valider at username og ikon for slack blir beholdt", func(t *testing.T) {
@@ -51,14 +54,15 @@ func TestReceivers(t *testing.T) {
 		}
 
 		alert := fixtures.AlertResource()
+		name := utils.GetCombinedName(alert)
 		alert.Spec.Receivers.Slack.SendResolved = boolp(false)
 		alert.Spec.Receivers.Slack.IconEmoji = ":fire:"
 		alert.Spec.Receivers.Slack.IconUrl = "https://url"
 		alert.Spec.Receivers.Slack.Username = "Username"
-		receiver := createReceiver(alert)
+		receiver := createReceiver(name, alert)
 		assert.Equal(t, "Username", receiver.SlackConfigs[0].Username)
 		assert.Equal(t, ":fire:", receiver.SlackConfigs[0].IconEmoji)
-		assert.Equal(t, "https://url", receiver.SlackConfigs[0].IconUrl)
-		assert.False(t, receiver.SlackConfigs[0].SendResolved)
+		assert.Equal(t, "https://url", receiver.SlackConfigs[0].IconURL)
+		assert.False(t, receiver.SlackConfigs[0].SendResolved())
 	})
 }
